8314e96736940edda2c27c0f85b169352eb10d3f
Guillaume1979 <guibetguillaume@gmail.com>
2023-11-20 23:04:24 +0100

    FEATURE: add header

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faRightToBracket} from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: 'header',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage, FontAwesomeModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  faLogin = faRightToBracket
+
+}
