857806b0eaf030fe16060cf79742601f967304cd
Guillaume1979 <guibetguillaume@gmail.com>
2023-12-12 00:02:40 +0100

    FEATURE: add routing to dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,26 +4,29 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     title: 'Tableau de bord',
-    loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    loadComponent: () =>
+      import('./components/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      ),
     //todo ajouter le auth guard (avec canMatch ?)
   },
-  {
-    path: 'welcome',
-    //todo à compléter, pour les connexions sans authentification
-    // ajouter un rapide descriptif du site et un bouton pour se connecter ou créer un compte
-  },
-  {
-    path:'not-authorized',
-    //todo à compléter
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    //todo à compléter. Page d'accueil du site quand non connecté
-    //ajouter une redirection vers dashboard si présence d'un token
-  },
+  // {
+  //   path: 'welcome',
+  //   //todo à compléter, pour les connexions sans authentification
+  //   // ajouter un rapide descriptif du site et un bouton pour se connecter ou créer un compte
+  // },
+  // {
+  //   path:'not-authorized',
+  //   //todo à compléter
+  // },
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   //todo à compléter. Page d'accueil du site quand non connecté
+  //   //ajouter une redirection vers dashboard si présence d'un token
+  // },
   {
     path: '**',
-    redirectTo: ''
-  }
+    redirectTo: '',
+  },
 ];
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,15 +2,15 @@ import {Component} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {faRightToBracket} from "@fortawesome/free-solid-svg-icons";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, FontAwesomeModule],
+  imports: [CommonModule, NgOptimizedImage, FontAwesomeModule, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  faLogin = faRightToBracket
-
+  faLogin = faRightToBracket;
 }
