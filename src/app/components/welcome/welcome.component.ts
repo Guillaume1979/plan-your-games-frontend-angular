54800d0d31f94e545e52e4048d9cee7b2ce4dea5
Guillaume1979 <guibetguillaume@gmail.com>
2023-12-18 18:31:15 +0100

    FEATURE: add content to the welcome component

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faExclamation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
 })
 export class WelcomeComponent {
+  discord = faDiscord;
+  exclamation = faExclamation;
 
+  login() {}
 }
