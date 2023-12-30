b8283720bd4519363f2723b227e61eeb65be0810
Guillaume1979 <guibetguillaume@gmail.com>
2023-12-30 20:05:40 +0100

    Add SessionService

---
+++ src/app/services/session.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, signal } from '@angular/core';
+import { Session } from '../models/session';
+import { Player } from '../models/player';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SessionService {
+  sessions = signal<Session[]>([
+    {
+      date: new Date(2022, 3, 1),
+      game: { name: 'Icarus' },
+      participants: [
+        new Player({ username: 'Alice' }),
+        new Player({ username: 'Guitou' }),
+        new Player({ username: 'Toto' }),
+      ],
+    },
+    {
+      date: new Date(2023, 11, 31),
+      game: { name: 'Mechabellum' },
+      participants: [
+        new Player({ username: 'Guitou' }),
+        new Player({ username: 'Kojak' }),
+      ],
+    },
+  ]);
+
+  constructor() {}
+}
