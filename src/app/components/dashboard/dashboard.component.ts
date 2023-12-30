3a0e6a1c53f03e221fcc3d07a136296be7355c4f
Guillaume1979 <guibetguillaume@gmail.com>
2023-12-30 20:06:05 +0100

    Update Dashboard component with session table
    Expanded functionality of the Dashboard component to display a table of sessions. The table was added by importing the CdkTableModule, integrating SessionService and iterating through sessions data. The component's HTML file has been enhanced to display various session details within the table.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CdkTableModule, DataSource } from '@angular/cdk/table';
+import { SessionService } from '../../services/session.service';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { Session } from '../../models/session';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CdkTableModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  sessionService = inject(SessionService);
+  displayedColumns = ['date', 'game', 'participants'];
+  sessions: Session[] = this.sessionService.sessions();
 
+  ngOnInit() {}
 }
