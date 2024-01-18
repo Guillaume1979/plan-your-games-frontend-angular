7f55c315a06364e7d7026e33650d3d2d3ee1ab1c
Guillaume1979 <guibetguillaume@gmail.com>
2024-01-18 19:11:55 +0100

    Replace login function with Discord authentication link
    The "join us" button's click function in the Welcome component has been replaced with a direct Discord authentication link. This change simplifies the user authentication process as it eliminates the need for a separate login function which was previously implemented.

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { faExclamation } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ENDPOINT_AUTH_DISCORD } from '../../../environment';
 
 @Component({
   selector: 'app-welcome',
@@ -13,6 +14,5 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 export class WelcomeComponent {
   discord = faDiscord;
   exclamation = faExclamation;
-
-  login() {}
+  protected readonly ENDPOINT_AUTH_DISCORD = ENDPOINT_AUTH_DISCORD;
 }
