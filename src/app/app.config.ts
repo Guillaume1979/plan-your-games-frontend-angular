e3a5f680d61867d3f1c7645289fa097307bb0266
Guillaume1979 <guibetguillaume@gmail.com>
2024-02-14 18:20:35 +0100

    Implement JWT token handling in HTTP interceptors
    The existing code in auth.interceptor.ts has been modified to handle JWT tokens from session storage. The changes will fetch the JWT token, if available, and clone the HTTP request to include an Authorization header. This should improve the handling of authenticated HTTP requests. The interceptor has also been updated in the app.config.ts.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import {
-  HTTP_INTERCEPTORS,
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
 };
--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -6,39 +6,22 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { JWT_TOKEN_KEY } from '../../environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(
-    req: HttpRequest<any>,
+    request: HttpRequest<any>,
     next: HttpHandler,
   ): Observable<HttpEvent<any>> {
-    return next.handle(req);
+    const token = sessionStorage.getItem(JWT_TOKEN_KEY);
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+    return next.handle(request);
   }
 }
-
-//exemple de code :
-// @Injectable()
-// export class AuthInterceptor implements HttpInterceptor {
-//   isAuthenticated = false;
-//
-//   constructor(private readonly authService: AuthService) {
-//     this.authService.isAuthenticated$.subscribe(
-//       (isAuthenticated) => (this.isAuthenticated = isAuthenticated)
-//     );
-//   }
-//
-//   intercept(
-//     request: HttpRequest<unknown>,
-//     next: HttpHandler
-//   ): Observable<HttpEvent<unknown>> {
-//     if (this.isAuthenticated) {
-//       request = request.clone({
-//         setHeaders: {
-//           Authorization: `Bearer ${this.authService.getRawToken()}`,
-//         },
-//       });
-//     }
-//     return next.handle(request);
-//   }
-// }
