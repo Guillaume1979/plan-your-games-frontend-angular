c07c2600bd23a82729292392623994cca811eb9e
Guillaume1979 <guibetguillaume@gmail.com>
2024-03-03 23:53:33 +0100

    Test: implement first test for the welcome component

--- src/app/components/welcome/welcome.component.spec.ts
+++ src/app/components/welcome/welcome.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WelcomeComponent } from './welcome.component';
+import { By } from '@angular/platform-browser';
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
@@ -8,10 +9,9 @@ describe('WelcomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WelcomeComponent]
-    })
-    .compileComponents();
-    
+      imports: [WelcomeComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,12 @@ describe('WelcomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a connection button', () => {
+    let connectionButton = fixture.debugElement.query(
+      By.css('[data-testingId="connection"]'),
+    );
+    expect(connectionButton).not.toBeNull();
   });
 });
