a8ca1349b2ec821c803ea4f3abac13d4c0f48fdd
Guillaume1979 <guibetguillaume@gmail.com>
2024-03-13 17:58:31 +0100

    Test: implement tests for logout method for AuthService

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { JWT_TOKEN_KEY } from '../../environment';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -15,5 +16,24 @@ describe('AuthService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('should log out properly', () => {
+    it('should remove the jwt token', () => {
+      sessionStorage.setItem(JWT_TOKEN_KEY, 'fakeJwt');
+      service.logout();
+      expect(sessionStorage.getItem(JWT_TOKEN_KEY)).toBeNull();
+    });
+
+    it('should disconnect the user', () => {
+      service.logout();
+      expect(service.isAuthenticated()).toBeFalse();
+    });
+
+    it('should navigate to the root url', () => {
+      const routerSpy = spyOn(service.router, 'navigateByUrl');
+      service.logout();
+      expect(routerSpy).toHaveBeenCalledWith('');
+    });
   });
 });
