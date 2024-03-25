a06738ddac8bf850093733eee0b7661810c18dc4
Guillaume1979 <guibetguillaume@gmail.com>
2024-03-25 19:37:06 +0100

    Feature: add matcher for all the protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,69 @@
-import { Routes } from '@angular/router';
+import { Route, Router, Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AuthRedirectComponent } from './auth/auth-redirect.component';
+import { inject } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+
+export const routesWithAuthentication = {
+  path: '',
+  children: [
+    {
+      path: 'dashboard',
+      title: 'Tableau de bord',
+      loadComponent: () =>
+        import('./components/dashboard/dashboard.component').then(
+          (m) => m.DashboardComponent,
+        ),
+      canMatch: [
+        () =>
+          inject(AuthService).isAuthenticated() ||
+          inject(Router).createUrlTree(['']),
+      ],
+      //todo ajouter le auth guard (avec canMatch ?)
+    },
+    {
+      path: 'sessions',
+      loadComponent: () =>
+        import('./components/session/session-list.component').then(
+          (m) => m.SessionListComponent,
+        ),
+    },
+  ],
+};
 
 export const routes: Routes = [
-  {
-    path: 'dashboard',
-    title: 'Tableau de bord',
-    loadComponent: () =>
-      import('./components/dashboard/dashboard.component').then(
-        (m) => m.DashboardComponent,
-      ),
-    //todo ajouter le auth guard (avec canMatch ?)
-  },
   {
     path: 'welcome',
     component: WelcomeComponent,
     //todo à compléter, pour les connexions sans authentification
     // ajouter un rapide descriptif du site et un bouton pour se connecter ou créer un compte
   },
-  {
-    path: 'sessions',
-    loadComponent: () =>
-      import('./components/session/session-list.component').then(
-        (m) => m.SessionListComponent,
-      ),
-  },
   // {
   //   path:'not-authorized',
   //   //todo à compléter
   // },
+  {
+    path: '',
+    canMatch: [() => inject(AuthService).isAuthenticated()],
+    children: [
+      {
+        path: 'dashboard',
+        title: 'Tableau de bord',
+        loadComponent: () =>
+          import('./components/dashboard/dashboard.component').then(
+            (m) => m.DashboardComponent,
+          ),
+        //todo ajouter le auth guard (avec canMatch ?)
+      },
+      {
+        path: 'sessions',
+        loadComponent: () =>
+          import('./components/session/session-list.component').then(
+            (m) => m.SessionListComponent,
+          ),
+      },
+    ],
+  },
   {
     path: `login`,
     component: AuthRedirectComponent,
