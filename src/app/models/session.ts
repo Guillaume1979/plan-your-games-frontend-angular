9dda07b2fac5aba3da025a16032e0c77616325f5
Guillaume1979 <guibetguillaume@gmail.com>
2024-03-26 18:29:06 +0100

    Refactor: update the models

--- src/app/models/session.ts
+++ src/app/models/session.ts
@@ -2,7 +2,8 @@ import { Game } from './game';
 import { User } from './user';
 
 export class Session {
-  id?: number;
+  uuid?: string;
+  title?: string;
   date = new Date();
   game = new Game();
   participants: User[] = [];
